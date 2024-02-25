2f55b6ddd0c8ed05cfde1df37614b42711954802
Michal Lopuszynski <michal.lopuszynski@tcl.com>
2024-02-25 23:54:55 +0100

    [trainer_llm] 0.0.5 Improve configuration options for model and data handling

--- examples/trainer_llm/configurator.py
+++ examples/trainer_llm/configurator.py
@@ -1,24 +1,42 @@
 from typing import Optional
-
+from typing_extensions import Annotated
 import pydantic
+
+
+DTYPES_PATTERN = r"^torch.float32$|^torch.bfloat16$|^torch.float16$"
 
 
 class DecomposeDWAINConfig(pydantic.BaseModel):
     task: str
-    decompose_model_name: str
+
+    # Model specification
+
+    decomposed_model_name: str
+    decomposed_model_dtype: Annotated[
+        str, pydantic.StringConstraints(pattern=DTYPES_PATTERN)
+    ]
+
+    # Data handling params
+
+    metric_max_length: int
+    metric_batch_size: int
+    data_max_length: int
+    data_batch_size: int
+
+    # Decomposition params
+
+    num_data_steps: int
+    num_metric_steps: int
     trade_off_factor: float
     proportion_threshold: float
     ppl_diff_threshold: float
     nsr_final_threshold: float
-    num_data_steps: int
-    num_metric_steps: int
-    blacklisted_module_names: Optional[list[str]] = None
     min_proportion: float
     min_rank: int
-    max_length: int
-    batch_size: int
+    blacklisted_module_names: Optional[list[str]] = None
 
-    # Finetuning
+    # Finetuning params
+
     run_finetuning: bool
     lora_finetuning: bool
     ft_lr: float = 0.0001
