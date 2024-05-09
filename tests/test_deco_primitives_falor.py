36b654e59c0484fba48285a49e9ed608e321dc3d
Michal Lopuszynski <michal.lopuszynski@tcl.com>
2024-05-09 22:48:47 +0200

    Clean-up test for falor decomposition primitives continued

--- tests/test_deco_primitives_falor.py
+++ tests/test_deco_primitives_falor.py
@@ -1,5 +1,6 @@
 import collections.abc
 
+import pytest
 import torch
 
 import ptdeco
@@ -9,12 +10,14 @@ from ptdeco.falor.decomposition import (
     _wrap_in_place,
 )
 
+torch.set_float32_matmul_precision("highest")
+
 
 def data_iterator_lin(
     bs: int, fin: int, h: int, w: int
 ) -> collections.abc.Generator[torch.Tensor, None, None]:
     gen = torch.Generator()
-    gen.manual_seed(314159)
+    gen.manual_seed(1314159)
     while True:
         yield torch.rand(bs, h, w, fin, generator=gen)
 
@@ -23,15 +26,23 @@ def data_iterator_conv(
     bs: int, fin: int, h: int, w: int
 ) -> collections.abc.Generator[torch.Tensor, None, None]:
     gen = torch.Generator()
-    gen.manual_seed(314159)
+    gen.manual_seed(1314159)
     while True:
         yield torch.rand(bs, fin, h, w, generator=gen)
 
 
 class MyNetworkLinear(torch.nn.Module):
-    def __init__(self, in_features: int, out_features: int):
+    def __init__(self, in_features: int, out_features: int, gen: torch.Generator):
         super().__init__()
         self.mod = torch.nn.Linear(in_features=in_features, out_features=out_features)
+        self.reset_parameters(gen)
+
+    def reset_parameters(self, gen: torch.Generator) -> None:
+        torch.nn.init.kaiming_uniform_(self.mod.weight, a=5**0.5, generator=gen)
+        if self.mod.bias is not None:
+            fan_in, _ = torch.nn.init._calculate_fan_in_and_fan_out(self.mod.weight)
+            bound = fan_in**-0.5 if fan_in > 0 else 0
+            torch.nn.init.uniform_(self.mod.bias, -bound, bound)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         y = self.mod(x)
@@ -39,11 +50,20 @@ class MyNetworkLinear(torch.nn.Module):
 
 
 class MyNetworkConv1x1(torch.nn.Module):
-    def __init__(self, in_features: int, out_features: int):
+    def __init__(self, in_features: int, out_features: int, gen: torch.Generator):
         super().__init__()
         self.mod = torch.nn.Conv2d(
             in_channels=in_features, out_channels=out_features, kernel_size=(1, 1)
         )
+        self.reset_parameters(gen)
+
+    def reset_parameters(self, gen: torch.Generator) -> None:
+        torch.nn.init.kaiming_uniform_(self.mod.weight, a=5**0.5, generator=gen)
+        if self.mod.bias is not None:
+            fan_in, _ = torch.nn.init._calculate_fan_in_and_fan_out(self.mod.weight)
+            if fan_in != 0:
+                bound = fan_in**-0.5
+                torch.nn.init.uniform_(self.mod.bias, -bound, bound, generator=gen)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         y = self.mod(x)
@@ -59,6 +79,8 @@ def get_output_before_and_after_decomposition(
     deco_rank: int,
 ) -> tuple[torch.Tensor, torch.Tensor]:
     x = next(data_iterator)
+    x = x.to(device)
+
     with torch.no_grad():
         y0 = root_module(x)
 
@@ -78,7 +100,6 @@ def get_output_before_and_after_decomposition(
             use_damping=True,
         )
 
-        deco_rank = 32
         uk = u[:, u.shape[1] - deco_rank :].to(torch.float)
         U, V = orig_weight.T @ uk, uk.T
         # deco_weight = (U @ V).T
@@ -99,14 +120,15 @@ def get_output_before_and_after_decomposition(
     return y0, y1
 
 
-def test_linear_decomposition_cpu() -> None:
+def get_output_before_and_after_decomposition_linear(
+    device: torch.device,
+) -> tuple[torch.Tensor, torch.Tensor]:
     dim_in = 64
     dim_out = 32
     dim_h, dim_w = 16, 16
-
-    device = torch.device("cpu")
-
-    m_lin = MyNetworkLinear(in_features=dim_in, out_features=dim_out)
+    gen = torch.Generator()
+    gen.manual_seed(271828)
+    m_lin = MyNetworkLinear(in_features=dim_in, out_features=dim_out, gen=gen)
     m_lin.to(device)
 
     di = data_iterator_lin(8, dim_in, dim_h, dim_w)
@@ -118,18 +140,20 @@ def test_linear_decomposition_cpu() -> None:
         data_iterator=di,
         deco_rank=min(dim_in, dim_out),
     )
+    return y0, y1
 
-    assert torch.max(torch.abs(y0 - y1).mean()) < 1.0e-6
 
-
-def test_conv1x1_decomposition_cpu() -> None:
+def get_output_before_and_after_decomposition_conv1x1(
+    device: torch.device,
+) -> tuple[torch.Tensor, torch.Tensor]:
     dim_in = 64
     dim_out = 32
     dim_h, dim_w = 16, 16
 
-    device = torch.device("cpu")
+    gen = torch.Generator()
+    gen.manual_seed(271828)
 
-    m_conv = MyNetworkConv1x1(in_features=dim_in, out_features=dim_out)
+    m_conv = MyNetworkConv1x1(in_features=dim_in, out_features=dim_out, gen=gen)
     m_conv.to(device)
 
     di = data_iterator_conv(8, dim_in, dim_h, dim_w)
@@ -141,5 +165,31 @@ def test_conv1x1_decomposition_cpu() -> None:
         data_iterator=di,
         deco_rank=min(dim_in, dim_out),
     )
+    return y0, y1
 
-    assert torch.max(torch.abs(y0 - y1).mean()) < 1.0e-6
+
+def test_linear_cpu() -> None:
+    device = torch.device("cpu")
+    y0, y1 = get_output_before_and_after_decomposition_linear(device)
+    assert torch.abs(y0 - y1).max() < 1.0e-6
+
+
+@pytest.mark.skipif(not torch.cuda.is_available(), reason="cuda not available")
+def test_linear_gpu() -> None:
+    device = torch.device("cuda")
+    y0, y1 = get_output_before_and_after_decomposition_linear(device)
+    assert torch.abs(y0 - y1).max() < 1.0e-6
+
+
+def test_conv1x1_cpu() -> None:
+    device = torch.device("cpu")
+    y0, y1 = get_output_before_and_after_decomposition_conv1x1(device)
+    assert torch.abs(y0 - y1).max() < 1.0e-6
+
+
+@pytest.mark.skipif(not torch.cuda.is_available(), reason="cuda not available")
+def test_conv1x1_gpu() -> None:
+    device = torch.device("cuda")
+    y0, y1 = get_output_before_and_after_decomposition_conv1x1(device)
+    # TODO: Very weird that gpu nees as high threshold to work
+    assert torch.abs(y0 - y1).max() < 9.0e-4
